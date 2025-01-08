24e7063a6f1c86ec35714a293e8e3f51c7582146
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-01-08 20:19:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstWord(s) {
   // your code here
+	s=s.trim();
+	if(s.length<1)
+	{
+		return ''
+	}
+	let j=0;
+	for(let i=0;i<s.length;i++)
+		{
+			if(s.charAt(i)==' ')
+			{
+				j=i;
+				break;
+			}
+		}
+	let ans=s.substring(0,j);
+	return ans;
 }
 
 // Do not change the code below
